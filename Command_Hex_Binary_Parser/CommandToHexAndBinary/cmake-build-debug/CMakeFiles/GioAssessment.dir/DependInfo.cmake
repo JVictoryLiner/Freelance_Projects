18a6bf9179ab7955ed7403488c61b823f224af7a
JVictoryLiner <61691146+JVictoryLiner@users.noreply.github.com>
2021-02-01 10:09:54 +0800

    Add files via upload

---
+++ Command_Hex_Binary_Parser/CommandToHexAndBinary/cmake-build-debug/CMakeFiles/GioAssessment.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/School/GioAssessment/taskOne.cpp" "D:/School/GioAssessment/cmake-build-debug/CMakeFiles/GioAssessment.dir/taskOne.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Command_Hex_Binary_Parser/CommandToHexAndBinary/cmake-build-debug/CMakeFiles/GioAssessment.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GioAssessment.dir/taskOne.cpp.obj"
+  "GioAssessment.exe"
+  "GioAssessment.exe.manifest"
+  "GioAssessment.pdb"
+  "libGioAssessment.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GioAssessment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
