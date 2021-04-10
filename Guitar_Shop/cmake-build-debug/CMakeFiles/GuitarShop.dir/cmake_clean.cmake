cbadcc181f0743be58bc5e27747506d6d7939070
JVictoryLiner <61691146+JVictoryLiner@users.noreply.github.com>
2021-04-10 15:15:55 +0800

    Add files via upload

---
+++ Guitar_Shop/cmake-build-debug/CMakeFiles/GuitarShop.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GuitarShop.dir/main.cpp.obj"
+  "GuitarShop.exe"
+  "GuitarShop.exe.manifest"
+  "GuitarShop.pdb"
+  "libGuitarShop.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GuitarShop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
